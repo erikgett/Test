02061d6d0f75c9fb9d986024f1a58ae420fa3177
erikgett <erikgette63@gmail.com>
2024-04-07 17:05:15 +0500

    Добавьте файлы проекта.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MaterialDesignColors;
+using MaterialDesignThemes.Wpf;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Purple_Click(object sender, RoutedEventArgs e)
+        {
+            SetPrimaryColor(Colors.MediumPurple, BaseTheme.Dark);
+        }
+
+        private void Green_Click(object sender, RoutedEventArgs e)
+        {
+            SetPrimaryColor(Colors.LightGreen, BaseTheme.Light);
+        }
+
+        private static void SetPrimaryColor(Color color, BaseTheme themeType = BaseTheme.Light)
+        {
+            PaletteHelper paletteHelper = new PaletteHelper();
+            var theme = paletteHelper.GetTheme();
+
+            //Apply primary color
+            theme.SetPrimaryColor(color);
+
+            theme.SetBaseTheme(themeType);
+
+            //Apply theme to application
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
